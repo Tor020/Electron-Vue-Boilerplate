cedc9c66a836d9f56dbf6207a3f27d7118550d21
Tor020 <kopp020@gmail.com>
2018-09-01 15:15:19 -0400

    default ahk commit message

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -36,7 +36,7 @@
   body {font-family:'Nunito';}
 
   #wrapper {
-    /* background: radial-gradient( ellipse at top left, rgba(255, 255, 255, 1) 40%, rgba(229, 229, 229, .9) 100% ); */
+    background: radial-gradient( ellipse at top left, rgba(255, 255, 255, 1) 40%, rgba(229, 229, 229, .9) 100% );
     height: 100vh;
     padding: 60px 80px;
     width: 100vw;
