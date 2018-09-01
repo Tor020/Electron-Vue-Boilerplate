fe557a34f7db299967818165057b10a47ec03fc3
Tor020 <kopp020@gmail.com>
2018-09-01 14:24:42 -0400

    default ahk commit message

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,11 +4,17 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 
+
+
+
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
+
 new Vue({
   components: { App },
   router,
