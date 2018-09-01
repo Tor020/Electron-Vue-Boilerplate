e00cb2e0e330200c68b0692249e034af936de6b6
Tor020 <kopp020@gmail.com>
2018-09-01 14:09:22 -0400

    default ahk commit message

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Landing from '@/components/LandingPage'
 
+
+// component: require('@/components/LandingPage').default
 Vue.use(Router)
 
 export default new Router({
@@ -8,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      component: Landing
     },
     {
       path: '*',
